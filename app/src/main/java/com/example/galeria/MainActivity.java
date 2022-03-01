64874f970a64e092ae0e3e003f4fd4478b76aa8b
fczasi <jetcrab893@gmail.com>
2022-03-01 15:49:30 +0100

    yespls

---
+++ app/src/main/java/com/example/galeria/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.galeria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ViewSwitcher;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageSwitcher simpleImageSwitcher;
+    private Button btnNext, btnBack;
+
+    int imageIds[] = {R.drawable.citrina, R.drawable.download, R.drawable.duck, R.drawable.img_lights};
+    int count = imageIds.length;
+    int currentIndex = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button gallery = findViewById(R.id.selectimg);
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 3);
+            }
+        });
+
+        btnNext = (Button) findViewById(R.id.next);
+        btnBack = findViewById(R.id.back);
+        simpleImageSwitcher = (ImageSwitcher) findViewById(R.id.podglad);
+        simpleImageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+
+            public View makeView() {
+                ImageView imageView = new ImageView(getApplicationContext());
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                imageView.setLayoutParams(new ImageSwitcher.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+                return imageView;
+            }
+        });
+        simpleImageSwitcher.setImageResource(imageIds[currentIndex]);
+
+        Animation in = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        Animation out = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+
+        simpleImageSwitcher.setInAnimation(in);
+        simpleImageSwitcher.setOutAnimation(out);
+
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                currentIndex++;
+                if (currentIndex == count)
+                    currentIndex = 0;
+                simpleImageSwitcher.setImageResource(imageIds[currentIndex]);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                currentIndex--;
+                if (currentIndex < 0)
+                    currentIndex = count-1;
+                simpleImageSwitcher.setImageResource(imageIds[currentIndex]);
+            }
+        });
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && data != null)
+        {
+            Uri selectedImage = data.getData();
+            ImageSwitcher wybierz =findViewById(R.id.podglad);
+            wybierz.setImageURI(selectedImage);
+        }
+    }
+}
